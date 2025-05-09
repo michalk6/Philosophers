1b248f798a071cba909eed2a59e845661966a4c9
Michal Kurek <michalkurek92@gmail.com>
2025-05-09 20:58:23 +0200

    arg error message

--- philo/srcs/philosophers.c
+++ philo/srcs/philosophers.c
@@ -6,22 +6,37 @@
 /*   By: mikurek <mikurek@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/03 19:10:42 by mikurek           #+#    #+#             */
-/*   Updated: 2025/05/09 17:35:24 by mikurek          ###   ########.fr       */
+/*   Updated: 2025/05/09 20:58:13 by mikurek          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
+
+int	ft_print_arg_error_message(void)
+{
+	printf(
+		"Incorrect input data\n"
+		"Expected arguments:\n"
+		"1: number of philosophers\n"
+		"2: time to die\n"
+		"3: time to eat\n"
+		"4: time to sleep\n"
+		"5: number of times each philosopher must eat (optional)\n"
+		"example: ./philo 42 600 200 200 [10]\n"
+		);
+	return (EXIT_FAILURE);
+}
 
 int	main(int argc, char **argv)
 {
 	t_table	*table;
 
 	if (argc != 5 && argc != 6)
-		return (0);
+		return (ft_print_arg_error_message());
 	table = ft_init_table(argc, argv);
 	if (table == NULL)
 		return (1);
 	ft_begin_feast(table->philos);
 	ft_free_table(table);
-	return (0);
+	return (EXIT_SUCCESS);
 }
